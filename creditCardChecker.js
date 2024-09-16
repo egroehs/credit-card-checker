5e15e45c82e4407e978377a6ec2338993fc41da6
egroehs <eduardagroehs@gmail.com>
2024-09-16 11:02:00 -0700

    idInvalidCardCompanies()

--- creditCardChecker.js
+++ creditCardChecker.js
@@ -11,7 +11,7 @@ const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
 const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
 const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
 const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
-
+const allInvalid = [invalid1, invalid2, invalid3, invalid4, invalid5];
 // Can be either valid or invalid
 const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
 const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
@@ -45,10 +45,30 @@ function findInvalidCards(arr) {
     const invalidCards = arr.filter(cards => !validateCred(arr))
     return invalidCards;
 }
+function idInvalidCardCompanies(invalidCards) {
+  const companies = {
+    3: "Amex (American Express)",
+    4: "Visa",
+    5: "Mastercard",
+    6: "Discover",
+  };
 
+  //using set to keep track of unique values
+  const companySet = new Set();
 
+  for (let card of invalidCards) {
+    let firstDigit = card[0];
 
-
-
+    if (companies[firstDigit]) {
+      companySet.add(companies[firstDigit]);
+    } else {
+      console.log("Company not found");
+    }
+  }
+  //convert Set to array
+  return Array.from(companySet);
+}
 console.log(validateCred(valid1));
 console.log(findInvalidCards(invalid2));
+
+console.log(idInvalidCardCompanies(allInvalid));
